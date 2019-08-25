a21d6ecb17c213e399cf9fc1db9e3eb7368deac5
bensigal <ben@bensigal.com>
2019-08-25 16:59:59 -0700

    Differentiated index and games, removed old links from /tunnel.node.js

--- www/tunnel.node.js
+++ www/tunnel.node.js
@@ -6,14 +6,8 @@ module.exports = function(req, res, server, whereis){
         case "righteous":
             server.getFile("righteous.html", req, res);
             break;
-        case "eggs":
-            server.redirect("https://www.youtube.com/watch?v=YlMang9B-wI?t=12s", req, res);
-            break;
         case "cantina":
             server.redirect("https://www.youtube.com/watch?v=FWO5Ai_a80M", req, res);
-            break;
-        case "raptor":
-            server.redirect("https://pre14.deviantart.net/3381/th/pre/f/2012/183/0/6/ronald_reagan_riding_a_velociraptor_by_sharpwriter-d55rsh7.jpg", req, res);
             break;
         case "meep":
             server.getFile("includes/meep.html", req, res);
@@ -21,12 +15,9 @@ module.exports = function(req, res, server, whereis){
         case "seconds":
             server.getFile("includes/seconds.html", req, res);
             break;
-        case "elo":
-            server.redirect("https://wismuth.com/elo/calculator.html", req, res);
-            break;
-        case "graveyard":
-            server.getFile("includes/graveyard.html", req, res);
-            break;
+		case "games":
+			server.getFile("includes/games.html", req, res);
+			break;
         default:
             server.defaultTunnel(req, res, "/");
             break;
