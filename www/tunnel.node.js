0271c7291a7098fdbc3adb2edf6864db57eab4bd
bensigal <ben@bensigal.com>
2019-08-28 17:57:06 -0700

    Changed linking system in /tunnel.node.js, continued work on homepages.

--- www/tunnel.node.js
+++ www/tunnel.node.js
@@ -1,29 +1,11 @@
 module.exports = function(req, res, server, whereis){
     
-    if(countSlashes(req.lastPath.substring(0, req.lastPath.length - 1)) <= 1){
-        switch(req.lastPath){
-        
-        case "righteous":
-            server.getFile("righteous.html", req, res);
-            break;
-        case "cantina":
-            server.redirect("https://www.youtube.com/watch?v=FWO5Ai_a80M", req, res);
-            break;
-        case "meep":
-            server.getFile("includes/meep.html", req, res);
-            break;
-        case "seconds":
-            server.getFile("includes/seconds.html", req, res);
-            break;
-		case "games":
-			server.getFile("includes/games.html", req, res);
-			break;
-        default:
-            server.defaultTunnel(req, res, "/");
-            break;
-        
-        }
-    }
+	server.defaultTunnel(req, res, "/", {links: {
+		meep: "includes/meep.html",
+		seconds: "includes/seconds.html",
+		games: "includes/games.html",
+		programming: "includes/programming.html"
+	}});
     
 }
 
